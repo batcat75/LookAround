e0a470c7e5afe4b480438dacb9bfc9870cc2d27f
Thitikorn <thitikorn_new@hotmail.com>
2015-03-31 10:53:46 +0700

    Test link button 2 pages

--- app/src/main/java/com/cmu/cpe/se/blarblarblar/lookaround/select_Shop.java
+++ app/src/main/java/com/cmu/cpe/se/blarblarblar/lookaround/select_Shop.java
@@ -1,38 +1,24 @@
 package com.cmu.cpe.se.blarblarblar.lookaround;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 
-public class Select_food_or_gadget extends ActionBarActivity {
+public class select_Shop extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_food_or_gadget);
-
-        ImageButton back_button = (ImageButton)findViewById(R.id.imageButtonBack);
-        back_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i =  new Intent(getApplicationContext(), MainActivity.class);
-                finish();
-            }
-        });
-
+        setContentView(R.layout.activity_select__shop);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_select_food_or_gadget, menu);
+        getMenuInflater().inflate(R.menu.menu_select__shop, menu);
         return true;
     }
 
